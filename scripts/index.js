6cd250cae0335f096a37cbeb439a52033536f0f0
bladeform <ahorabbi@yandex.ru>
2024-03-08 00:13:55 +0300

    realised js function

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsList = document.querySelector('.places__list');
+const cardsTemplate = document.querySelector('#card-template').content;
+
+function foramateCard(item, deleteCard) {
+    let card = cardsTemplate.cloneNode(true);
+    card.querySelector('.card__image').src = item.link;
+    card.querySelector('.card__title').textContent = item.name;
+    card.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return card;
+}
+
+function deleteCard(e) {
+    const removeCard = e.target.closest('.card');
+    removeCard.remove(); 
+}
+
+initialCards.forEach(element => {
+    cardsList.append(foramateCard(element, deleteCard));
+});
